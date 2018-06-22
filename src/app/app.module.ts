e87bfc6f98806c6900f87ad921f62aa912c7d1db
Rachel <rshalom1984@gmail.com>
2018-06-22 13:24:25 +0300

    refactor root app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-
+import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
+// this is a decorator. everything that starts with an @ is a decorator for example, 
+//NgModule receives the AppModule class and adds to it methods 
+//and members that later on will be used by Angular. 
+//This way, Angular will recognize that this class represents an NgModule.
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InputButtonUnitComponent
   ],
   imports: [
     BrowserModule
