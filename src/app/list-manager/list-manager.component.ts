a7f5c65faf3ec25e24ac4d25817ace61a72a670e
Rachel <rshalom1984@gmail.com>
2018-09-30 10:06:07 +0200

    refactor app component: mov e logic from app to list manager component

--- src/app/list-manager/list-manager.component.ts
+++ src/app/list-manager/list-manager.component.ts
@@ -1,11 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-list-manager',
-  templateUrl: './list-manager.component.html',
+  selector: 'todo-list-manager',
+  template:`<div style="text-align:center">
+  <h1>
+      Welcome to {{secondaryTitle}} !
+  </h1>
+  <h2>My to do List</h2>
+  
+  <app-input-button-unit (submitTodo)="addTodo($event)"></app-input-button-unit>
+  <ul>
+    <li *ngFor= "let todo of todoList"><todo-item [itemTitle]="todo"></todo-item></li>
+  </ul>
+
+</div>`,
   styleUrls: ['./list-manager.component.css']
 })
 export class ListManagerComponent implements OnInit {
+    secondaryTitle = "Our MoinWorld and ngGrils";
+    todoList = [{ title: "complete angular todo-list project" }]
+
+    addTodo(newTodo: string): void {
+        this.todoList.push({ title: newTodo });
+    }
+
 
   constructor() { }
 
