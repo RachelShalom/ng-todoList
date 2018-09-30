cf3fb12ab7f5e972b700f8507abc9457dc238c55
Rachel <rshalom1984@gmail.com>
2018-09-30 09:58:40 +0200

    generate list manager component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { InputButtonUnitComponent } from './input-button-unit/input-button-unit.component';
 import { ItemComponent } from './item/item.component';
+import { ListManagerComponent } from './list-manager/list-manager.component';
 // this is a decorator. everything that starts with an @ is a decorator for example, 
 //NgModule receives the AppModule class and adds to it methods 
 //and members that later on will be used by Angular. 
@@ -12,7 +13,8 @@ import { ItemComponent } from './item/item.component';
   declarations: [
     AppComponent,
     InputButtonUnitComponent,
-    ItemComponent
+    ItemComponent,
+    ListManagerComponent
   ],
   imports: [
     BrowserModule
