cecd442631a9de7dbdf11d570f2cbba1c563eded
Rachel <rshalom1984@gmail.com>
2018-09-30 08:35:55 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
+  selector: 'todo-root',
+  template: `<div style="text-align:center">
+  <h1>
+      Welcome to {{secondaryTitle}} !
+  </h1>
+  <h2>My to do List</h2>
+  
+  <app-input-button-unit></app-input-button-unit>
+</div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
-  x="ngGrils";
+  secondaryTitle="Our MoinWorld and ngGrils";
 }
