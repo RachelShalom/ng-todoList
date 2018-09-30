e5697c366fc2a720a462b2a2e21f76fd0b34de03
Rachel <rshalom1984@gmail.com>
2018-09-30 09:53:07 +0200

    add @output event emitter to input component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,17 +2,28 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'todo-root',
-  template: `<div style="text-align:center">
+    template: `
+<div style="text-align:center">
   <h1>
       Welcome to {{secondaryTitle}} !
   </h1>
   <h2>My to do List</h2>
   
-  <app-input-button-unit></app-input-button-unit>
+  <app-input-button-unit (submitTodo)="addTodo($event)"></app-input-button-unit>
+  <ul>
+    <li *ngFor= "let todo of todoList"><todo-item [itemTitle]="todo"></todo-item></li>
+  </ul>
+
 </div>`,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
   title = 'app';
   secondaryTitle="Our MoinWorld and ngGrils";
+  todoList = [{title:"complete angular todo-list project"}]
+
+  addTodo(newTodo: string):void {
+    this.todoList.push({title:newTodo});
+}
+
 }
